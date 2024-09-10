4b1ca7f20061ab9c86b93118010227fe619577d8
Zach Magloughlin <zmagloughlin@mines.edu>
2024-09-09 18:53:14 -0600

    Added Projects section and Projects menu. Working on getting content to show per menu item

---
+++ webpage/index.js
@@ -0,0 +1,17 @@
+
+//Projects - Software Engineering
+document.getElementById("swe").onclick = function() {show_swe_content()};
+function show_swe_content() {
+  document.getElementById("swe_content").style.display = 'visible';
+}
+
+//Projects - Web Development
+
+
+//Projects - Data Analysis & Visualization
+
+
+//Projects - Databases
+
+
+//Projects - Game Development
