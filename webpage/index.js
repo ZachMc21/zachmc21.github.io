22d6d75770f55367382323a1cef1ae1f45a1158d
Zach Magloughlin <zmagloughlin@mines.edu>
2024-09-11 17:41:39 -0600

    Still working on getting the Projects section to show/hide when menu buttons are clicked

--- webpage/index.js
+++ webpage/index.js
@@ -1,25 +1,39 @@
 
-var project_categories = document.getElementsByClassName("project_content");
-const num_project_categories = project_categories.length;
+let project_menu_items = document.getElementById("projects_menu").childNodes;
+const num_project_categories = project_menu_items.length;
 
-window.onload = function() {reset_displayed_content()};
+window.onload = function() { reset_displayed_content() };
+
 
 function show_content(element) {
     reset_displayed_content();
+
+    //Set current element to be visible and an active menu
     element.style.display = 'visible';
+    element.classList.add('active');
 }
 
 //Clear content of projects other than the one selected
 function reset_displayed_content() {
+    var content_list = document.getElementsByClassName("project_content");
+    //Set content items to 'display: none'
     for (let i=0; i<num_project_categories; i++) {
-        project_categories[i].style.display = 'none';
+        content_list[i].style.display = 'none';
+    }
+    
+    //Remove active class from all menu links
+    for (let i=0; i<num_project_categories; i++) {
+        var isActive = project_menu_items.item(i).hasClass('active');
+        if (isActive) {
+            project_menu_items[i].classList.remove("active");
+        }
     }
 }
 
 //Projects - Software Engineering
 function swe() {
-    var element = document.getElementById("swe_content");
-    document.getElementById("swe").onclick = function() {show_content(element)};
+    var content = document.getElementById("swe_content");
+    document.getElementById("swe").onclick = function() { show_content(content) };
 }
 
 //Projects - Web Development
