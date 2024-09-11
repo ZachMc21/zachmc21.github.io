c00d80dfecb6b1a3eb575d836c45e9c6f7a5ed36
Zach Magloughlin <zmagloughlin@mines.edu>
2024-09-11 04:48:52 -0600

    WOrking on JS to get Projects to show/hide when menu item is clicked

--- webpage/index.js
+++ webpage/index.js
@@ -1,17 +1,42 @@
 
+var project_categories = document.getElementsByClassName("project_content");
+const num_project_categories = project_categories.length;
+
+window.onload = function() {reset_displayed_content()};
+
+function show_content(element) {
+    reset_displayed_content();
+    element.style.display = 'visible';
+}
+
+//Clear content of projects other than the one selected
+function reset_displayed_content() {
+    for (let i=0; i<num_project_categories; i++) {
+        project_categories[i].style.display = 'none';
+    }
+}
+
 //Projects - Software Engineering
-document.getElementById("swe").onclick = function() {show_swe_content()};
-function show_swe_content() {
-  document.getElementById("swe_content").style.display = 'visible';
+function swe() {
+    var element = document.getElementById("swe_content");
+    document.getElementById("swe").onclick = function() {show_content(element)};
 }
 
 //Projects - Web Development
+function webdev() {
+}
 
 
 //Projects - Data Analysis & Visualization
+function data() {
+}
 
 
 //Projects - Databases
+function database() {
+}
 
 
 //Projects - Game Development
+function gamedev() {
+}
