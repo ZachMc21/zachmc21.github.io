5d79951a56e270d7211da9bbafa2b1687356435b
Zach Magloughlin <zmagloughlin@mines.edu>
2024-09-13 16:18:34 -0600

    Tried to rework index.js, some progress, decided to restart. index.js renamed to index-old.js. Beginning to rebuild index.js from the start

--- webpage/index.js
+++ webpage/index.js
@@ -1,69 +1,22 @@
+let activeContent = "";
 
-
-//TODO set this to be not hard-coded
-let project_menu_names = ["swe", "webdev", "data", "database", "gamedev"]
-let project_content_names = ["swe_content", "webdev_content", "data_content", "database_content", "gamedev_content"]
-const num_project_categories = project_menu_names.length;
-
-
-window.onload = function() { reset_displayed_content() };
-
-
-// TODO: rework with cleaner text/element_name management
-function show_content(element) {
-    reset_displayed_content();
-
-    //Set current element to be visible
-    element.style.display = 'block';
-    //Set current element's menu entry to be active
-    element_name_with_content = element.getAttribute("id")
-    slice_index = element_name_with_content.indexOf('_');
-    element_name = element_name_with_content.slice(0, slice_index);
-    document.getElementById(element_name).classList.add('active');
+window.onload = function() {
+    const projectMenu = document.getElementById("project_menu");
+    const projectMenuLinks = projectMenu.children;
+    const projectContentList = document.getElementsByClassName("project_content");
+    const numProjectCategories = projectMenuLinks.length;
 }
 
-//Clear content of projects other than the one selected
-function reset_displayed_content() {
-    //Set content items to 'display: none'
-    for (let i=0; i<num_project_categories; i++) {
-        document.getElementById(project_content_names[i]).style.display = 'none';
-    }
-    
-    //Remove active class from all menu links
-    for (let i=0; i<num_project_categories; i++) {
-        current_menu_item = document.getElementById(project_menu_names[i]);
-        if (current_menu_item.classList.contains("active")) {
-            current_menu_item.classList.remove("active");
+function resetProjectMenu() {
+    for (let i=0; i<numProjectCategories; i++) {
+        if (projectMenuLinks[i].classList.contains("active")) {
+            projectMenuLinks[i].classList.remove("active");
         }
     }
 }
 
-//Projects - Software Engineering
-function swe() {
-    var content = document.getElementById("swe_content");
-    document.getElementById("swe").onclick = function() { show_content(content) };
-}
-
-//Projects - Web Development
-function webdev() {
-    var content = document.getElementById("webdev_content");
-    document.getElementById("webdev").onclick = function() { show_content(content) };
-}
-
-//Projects - Data Analysis & Visualization
-function data() {
-    var content = document.getElementById("data_content");
-    document.getElementById("data").onclick = function() { show_content(content) };
-}
-
-//Projects - Databases
-function database() {
-    var content = document.getElementById("database_content");
-    document.getElementById("database").onclick = function() { show_content(content) };
-}
-
-//Projects - Game Development
-function gamedev() {
-    var content = document.getElementById("gamedev_content");
-    document.getElementById("gamedev").onclick = function() { show_content(content) };
+function resetProjectContent() {
+    for (let i=0; i<numProjectCategories; i++) {
+        projectContentList[i].style.display="none";
+    }
 }
